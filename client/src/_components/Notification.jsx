d5edc600cd1cf3ad6686caebda9cb35991541a4a
Dmitry Mikhalchenko <dm.mail@tut.by>
2018-09-13 16:17:57 +0300

    Move close btn on notification area to the right

--- client/src/_components/Notification.jsx
+++ client/src/_components/Notification.jsx
@@ -14,7 +14,7 @@ class Notification extends React.Component {
     return (
       <div className={'notification ' + (alert.message ? 'fadein': 'fadeout invisible')}>
         {alert.message &&
-        <div className={`d-flex align-items-between  alert ${alert.type || ''}`}>
+        <div className={`d-flex justify-content-between align-items-between  alert ${alert.type || ''}`}>
           <span>{alert.message}</span>
           <span aria-hidden="true" onClick={this.cancel} className="close-btn">&times;</span>
         </div>}
