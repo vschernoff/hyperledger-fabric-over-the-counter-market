35d97614a3abd43026efc09964e1e186279726c9
Aliaksandr Krasitski <aliaksandr.krasitski@altoros.com>
2018-10-08 11:01:52 +0300

    update dependencies

---
+++ client/build/precache-manifest.57cd5eba10bb2aa090c1b6645237eaf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0f30423607f3083bd394",
+    "url": "./static/js/main.0f304236.chunk.js"
+  },
+  {
+    "revision": "2d417ab46f8e38885017",
+    "url": "./static/js/1.2d417ab4.chunk.js"
+  },
+  {
+    "revision": "0f30423607f3083bd394",
+    "url": "./static/css/main.c66604ba.chunk.css"
+  },
+  {
+    "revision": "2d417ab46f8e38885017",
+    "url": "./static/css/1.ee05fe6c.chunk.css"
+  },
+  {
+    "revision": "54eb7eae7d4edfafbff1c499a380c058",
+    "url": "./index.html"
+  }
+];
